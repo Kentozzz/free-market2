bc74968300a24f046c884392a850971833172608
kentozzz <kento.sobayashi@di-v.co.jp>
2020-08-20 17:45:57 +0900

    商品購入

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,9 @@ Rails.application.routes.draw do
 #   end
 #   resources :cards, only: [:index, :new]
 # end
-resources :cards, only: [:new, :index, :create, :destroy]
-
+resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]  do
+  member do
+    get "purchase_confirmation"
+    post "purchase"  ## 追加
+  end
 end
